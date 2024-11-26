5aa126e71ab3c5be32b41be6d0d6ded93935c45c
BTFitti <bruno.fitti734@live.com>
2024-11-26 17:26:51 -0300

    Created social components
    Creating a component that shows the social icons, to show the social icons i used react-icons

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,30 @@
-export function Home(){
-    return(
-        <div>
-            <h1>Pagina Home</h1>
-        </div>
-    )
+import { Social } from "../../components/social";
+import { FaLinkedin, FaGithub, FaLink } from "react-icons/fa";
+export function Home() {
+  return (
+    <div className="flex flex-col w-full py-4 items-center justify-center">
+      <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">
+        Bruno Fitti
+      </h1>
+      <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
+
+      <main className="flex flex-col w-11/12 max-w-xl text-center">
+        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+          <a>
+            <p className="text-base md:text-lg">Canal no Youtube</p>
+          </a>
+        </section>
+
+        <footer className="flex justify-center gap-3 my-4">
+          <Social url="https://github.com/BTFitti">
+            <FaGithub size={35} color="#fff"/>
+          </Social>
+          <Social url="https://www.linkedin.com/in/bruno-fitti-0629251b9/">
+            <FaLinkedin size={35} color="#fff"/>
+          </Social>
+        </footer>
+
+      </main>
+    </div>
+  );
 }
