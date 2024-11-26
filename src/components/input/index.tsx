06d981404ff603b0533bd3065704d6086f1b1f51
BTFitti <bruno.fitti734@live.com>
2024-11-26 18:53:51 -0300

    Creating the input component & login page
    By creating the input component, i can reuse it on different pages of my application. And i can apply styles with tailwind.

---
+++ src/components/input/index.tsx
@@ -0,0 +1,27 @@
+import { InputHTMLAttributes, useState } from "react"
+//A tipagem InputProps está extendendo todas as propriedades que o tipo input recebe.
+
+/*
+InputHTMLAttributes: Este tipo é importado do React e representa as propriedades padrão que um elemento <input> HTML pode ter. 
+Isso inclui atributos como type, value, placeholder, onChange, etc.
+
+Extensão de InputHTMLAttributes: A interface InputProps estende InputHTMLAttributes<HTMLInputElement>. 
+Isso significa que qualquer propriedade válida para um elemento <input> HTML também é válida para o componente Input.
+
+
+O componente Input recebe um objeto props do tipo InputProps. 
+Isso significa que o componente pode receber qualquer propriedade válida de um elemento <input> ou seja, pode receber um 
+type, value, placeholder, onChange.
+
+
+*/
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{}
+
+export function Input(props: InputProps){
+    return(
+        <input 
+        className="  border-0 h-9 rounded-md mb-5 px-3 outline-none" 
+        {...props}
+        />
+    )
+}
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,19 +1,49 @@
-
-import { useNavigate } from "react-router-dom";
+import { FormEvent, useState } from "react";
+import { Input } from "../../components/input";
+import { Link, useNavigate } from "react-router-dom";
 export function Login() {
   const navigate = useNavigate();
-  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  function handleSubmit() {
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
     navigate("/admin");
   }
   return (
-    <main >
-      <form action="" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Digite seu email" />
-        <input type="text" placeholder="Digite sua senha" />
-        <button>Acessar</button>
+    <div className="flex w-full h-screen items-center justify-center flex-col">
+      <Link to={"/"}>
+        <h1 className="mt-11 text-white mb-7 font-bold text-5xl">
+          Dev
+          <span className="bg-gradient-to-r from-emerald-300 to-sky-500 bg-clip-text text-transparent ">
+            Link
+          </span>
+        </h1>
+      </Link>
+      {/* Ativando o display flex, os elementos dentro do form pegam a largura de 100% */}
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-xl flex flex-col px-"
+      >
+        <Input
+          placeholder="Digite seu email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Digite sua senha"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="h-11 bg-gradient-to-tl from-sky-800 to-emerald-300 rounded-lg border-0 text-xl p-1 font-medium text-white "
+        >
+          Acessar
+        </button>
       </form>
-    </main>
+    </div>
   );
 }
